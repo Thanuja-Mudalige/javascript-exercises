11a9f5bd6c61dbc14747fb14e6fb66832f47ac94
Thanuja Mudalige <265001976+Thanuja-Mudalige@users.noreply.github.com>
2026-06-30 14:34:53 +0530

    do palindromes assignment

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+let textlower=text.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+let revesed=textlower.split('').reverse().join('');
+if(textlower==revesed){
+    return true;
+}
+else{
+    return false;
+}
 };
 
 // Do not edit below this line
