c19ae23d27bee6d6a0a51a887b52a3e896d483d4
Thanuja Mudalige <265001976+Thanuja-Mudalige@users.noreply.github.com>
2026-06-30 15:35:30 +0530

    Do fibonacci assignment

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+let firstCharacter=1;
+let secondCharacter=1;
+let nextCharacter;
+if(num<0){return 'OOPS'}
+else if(num==0){return 0;}
+else if(num==1||num==2){
+    return 1;
+}
+else{
+for(let i=3; i<=num;i++){
+nextCharacter=firstCharacter+secondCharacter;
+firstCharacter=secondCharacter;
+secondCharacter=nextCharacter;
+}}
+return nextCharacter;
 };
 
 // Do not edit below this line
