3cc357ea1a74345498f20358312c4e9bf1efbb0f
Thanuja Mudalige <265001976+Thanuja-Mudalige@users.noreply.github.com>
2026-06-30 16:21:20 +0530

    Do findTheOldest addignment

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+let newArray=people.map((element)=>{
+    let name=element.name;
+    let age=(element.yearOfDeath || new Date().getFullYear())-element.yearOfBirth;
+    return {name,age}
 
+});
+return newArray.reduce((prev,current)=>prev.age>current.age?prev:current)
 };
 
 // Do not edit below this line
