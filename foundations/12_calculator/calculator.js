03f4f65a80926d96199583e78436ad43dd0adace
Thanuja Mudalige <265001976+Thanuja-Mudalige@users.noreply.github.com>
2026-06-30 13:29:47 +0530

    do calculator OP foundation assignment

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+ let resault=numbers.reduce((sum,current)=>sum+current,0);
+ return resault;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+ let resault=numbers.reduce((sum,current)=>sum*current,1);
+ return resault;
 };
 
-const power = function() {
-	
+const power = function(base,exponent) {
+	return Math.pow(base,exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num==1||num==0){
+    return 1;
+  }
+  else{
+    let result=1;
+	for(let i=2;i<=num;i++){
+    
+    result=result*i;
+  }
+   return result;
+}
 };
 
 // Do not edit below this line
